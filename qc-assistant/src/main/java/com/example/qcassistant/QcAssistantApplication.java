56b74fc18b49c2c4fe3b91cc8fc0437b87d3e954
VKTRNDRV <viktor.androv039@gmail.com>
2023-09-04 00:55:43 +0300

    test commit

--- qc-assistant/src/main/java/com/example/qcassistant/QcAssistantApplication.java
+++ qc-assistant/src/main/java/com/example/qcassistant/QcAssistantApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QcAssistantApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(QcAssistantApplication.class, args);
     }
 
