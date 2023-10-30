e61658cf8e3f55848dade282e2b46eda6bad40ab
VKTRNDRV <viktor.androv039@gmail.com>
2023-10-30 23:27:54 +0200

    attempt 4

--- qc-assistant/src/main/java/com/example/qcassistant/Main.java
+++ qc-assistant/src/main/java/com/example/qcassistant/Main.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QcAssistantApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(QcAssistantApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
